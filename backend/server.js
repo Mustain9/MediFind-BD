150fe67fe3eff6508429553b8449a803098f3773
mustain9 <mustainbellah@gmail.com>
2026-08-21 21:23:09 +0600

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -114,6 +114,6 @@ app.get("/api/profile", verifyToken, (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server Running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`🚀 Server Running on port ${PORT}`);
 });
